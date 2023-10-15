5a503820bf9ca1aceff1c51361c1870548cd9f53
syeda0109 <syed.new.website@gmail.com>
2023-10-15 16:15:07 +0300

    init

---
+++ import.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == UPLOAD_ERR_OK) {
+    $file = $_FILES['csv_file']['tmp_name'];
+    $csvFile = $file;
+    $count = 0;
+
+    $html = '<table border="1">';
+    $html .= '<thead>';
+    
+    if (($handle = fopen($csvFile, "r")) !== false) {
+        for ($i=0; $i<19; $i++) {
+            $html .= '<tr>';
+            $count = 0;
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                
+                if ($count < 5) {
+                    $html .= '<td>' . $data[$i] . '</td>';
+                }
+
+                $count++;
+
+                if($count == 5) {
+                    break;
+                }
+            }
+            rewind($handle);
+            $html .= '</tr>';
+        }
+        if (isset($handle)) {
+            fclose($handle);
+        }
+    }
+    
+    $html .= '</table>';
+    echo $html;
+}
+?>
