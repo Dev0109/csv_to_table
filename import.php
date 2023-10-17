8b4962eb1ac43fea8dca9e372437cd6eb3a17b5b
dev0109 <devtop0109@gmail.com>
2023-10-17 13:31:14 +0300

    update

--- import.php
+++ import.php
@@ -1,7 +1,9 @@
 <?php
 if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == UPLOAD_ERR_OK) {
     $file = $_FILES['csv_file']['tmp_name'];
+    $file_name = $_FILES['csv_file']['name'];
     $csvFile = $file;
+
     $count = 0;
 
     $html = '<table border="1">';
@@ -47,5 +49,113 @@ if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == UPLOAD_ERR_OK)
     
     $html .= '</table>';
     echo $html;
+
+    // ... CSV processing code (as in your original code) ...
+
+    // Check for form submission
+    if (isset($_POST['submit'])) {
+
+        // Database connection settings
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "aca1_db1";
+
+        // Create a connection
+        $mysqli = new mysqli($hostname, $username, $password, $database);
+
+        // Check the connection
+        if ($mysqli->connect_error) {
+            die("Connection failed: " . $mysqli->connect_error);
+        }
+
+        if (isset($_POST['site_name']) && isset($_POST['dnsp']) && isset($_POST['retailer']) && isset($_POST['description'])) {
+            $site_name = $mysqli->real_escape_string($_POST['site_name']);
+            $dnsp = $mysqli->real_escape_string($_POST['dnsp']);
+            $retailer = $mysqli->real_escape_string($_POST['retailer']);
+            $description = $mysqli->real_escape_string($_POST['description']);
+
+            $table_name = "p004_site";
+            $site_id = "";
+            $dnsp_id = "";
+            $retailer_id = "";
+
+            // Insert data into the database
+            $sql_site = "INSERT INTO `$table_name` (site_name) VALUES ('$site_name')";
+
+            if ($mysqli->query($sql_site) === TRUE) {
+                echo "Record inserted successfully";
+            } else {
+                echo "Error: " . $sql_site . "<br>" . $mysqli->error;
+            }
+
+            $sql_site = "SELECT id FROM p004_site WHERE site_name = ?";
+            $stmt = $mysqli->prepare($sql_site);
+            $stmt->bind_param("s", $site_name);
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+                
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $site_id = $row['id'];
+                    echo "ID for data value '$site_name' is $site_id";
+                } else {
+                    echo "No matching data value found.";
+                }
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+
+            $sql_dnsp = "SELECT id FROM p003_dnsp WHERE dnsp_name = ?";
+            $stmt = $mysqli->prepare($sql_dnsp);
+            $stmt->bind_param("s", $dnsp);
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+                
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $dnsp_id = $row['id'];
+                    echo "ID for data value '$dnsp' is $dnsp_id";
+                } else {
+                    echo "No matching data value found.";
+                }
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+
+            $sql_retailer = "SELECT id FROM p004_retailer WHERE retailer_code = ?";
+            $stmt = $mysqli->prepare($sql_retailer);
+            $stmt->bind_param("s", $retailer);
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+                
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $retailer_id = $row['id'];
+                    echo "ID for data value '$retailer' is $retailer_id";
+                } else {
+                    echo "No matching data value found.";
+                }
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+        }
+
+        $table_name = "p004_nmi_batch";
+
+            // Insert data into the database
+        $sql_site = "INSERT INTO `$table_name` (site_id, dnsp_id, retailer_id, nmi_csv, batch_description) VALUES ('$site_id', '$dnsp_id', '$retailer_id', '$file_name', '$description')";
+
+        if ($mysqli->query($sql_site) === TRUE) {
+            echo "Record inserted successfully";
+        } else {
+            echo "Error: " . $sql_site . "<br>" . $mysqli->error;
+        }
+
+        // Close the database connection
+        $mysqli->close();
+    } else {
+        echo "Form not submitted.";
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,94 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CSV File Import</title>
+    <title>NMI Batch Loader</title>
 </head>
 <body>
-    <h1>CSV File Import</h1>
+    <h1>NMI Batch Loader</h1>
     <form action="import.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="csv_file" accept=".csv">
-        <input type="submit" value="Upload and Import">
+        <div>
+            <h1>CSV File Import</h1>
+            <input type="file" name="csv_file" accept=".csv" required>
+        </div>
+        <div>
+            <h1>Enter the Site Name</h1>
+            <input type="text" name="site_name" required>
+        </div>
+        <div>
+        <?php
+            $hostname = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "aca1_db1";
+
+            $mysqli = new mysqli($hostname, $username, $password, $database);
+
+            $query_dnsp = "SELECT * FROM p003_dnsp";
+
+            $result_dnsp = $mysqli->query($query_dnsp);
+            $dnsp_array = []; // Corrected variable name
+
+            if ($result_dnsp) {
+                if ($result_dnsp->num_rows > 0) {
+                    while ($row = $result_dnsp->fetch_assoc()) {
+                        array_push($dnsp_array, $row['dnsp_name']);
+                    }
+                } else {
+                    echo "No rows found in the table.";
+                }
+
+                $result_dnsp->close();
+            } else {
+                echo "Query failed: " . $mysqli->error;
+            }
+
+            $query_retailer = "SELECT * FROM p004_retailer"; // This should be querying retailer table
+
+            $result_retailer = $mysqli->query($query_retailer);
+            $retailer_name = [];
+            $retailer_code = [];
+
+            if ($result_retailer) {
+                if ($result_retailer->num_rows > 0) {
+                    while ($row = $result_retailer->fetch_assoc()) {
+                        array_push($retailer_name, $row['retailer_name']);
+                        array_push($retailer_code, $row['retailer_code']);
+                    }
+                } else {
+                    echo "No rows found in the table.";
+                }
+
+                $result_retailer->close();
+            } else {
+                echo "Query failed: " . $mysqli->error;
+            }
+
+            $mysqli->close();
+        ?>
+            <h1>Select DNSP</h1>
+            <select id="dnsp" name="dnsp" required>
+                <?php
+                for ($i = 0; $i < count($dnsp_array); $i++) {
+                    echo '<option value="' . $dnsp_array[$i] . '">' . $dnsp_array[$i] . '</option>';
+                }
+                ?>
+            </select>
+        </div>
+        <div>
+            <h1>Select Retailer</h1>
+            <select id="retailer" name="retailer" required>
+                <?php
+                for ($i = 0; $i < count($retailer_name); $i++) {
+                    echo '<option value="' . $retailer_code[$i] . '">' . $retailer_name[$i] . '</option>';
+                }
+                ?>
+            </select>
+        </div>  
+        <div>
+            <h1>Enter the Description</h1>
+            <textarea type="text" name="description" rows="4" cols="50" required></textarea>
+        </div>       
+        <input type="submit" name="submit" value="Submit" style="margin-top: 10px">
     </form>
 </body>
 </html>
-
